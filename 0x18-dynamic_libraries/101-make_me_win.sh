37d44c6a15d19d8461e9f5f4bd1db38317c670b2
Nkagisang <goeieman7@gmail.com>
2023-09-08 04:41:19 -0700

    C - Hash tables

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://github.com/nkagisanggoeieman/alx/raw/master/0x18-Dynamic-libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,4 @@
+C - Hash tables
+C
+Algorithm
+Data structure
